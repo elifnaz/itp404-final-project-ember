96716c25aa82eb275afde1ad120152bb7a863be4
Elif Aydin <eaydin@usc.edu>
2016-12-09 12:43:59 -0800

    controller fix

--- client/app/controllers/search.js
+++ client/app/controllers/search.js
@@ -9,12 +9,22 @@ export default Ember.Controller.extend({
       console.log('form submitted');
       var searchTerm = this.get('searchTerm');
       var location = this.get('location');
+      if (searchTerm == null) {
+        searchTerm = 'pizza';
+      }
+      if (location == null) {
+        location = 'random';
+      }
       this.transitionToRoute('search.results', searchTerm, location);
     },
     searchRandom: function(e) {
       e.preventDefault();
       console.log('random form submitted');
       var searchTerm = this.get('searchTerm');
+      if (searchTerm == null) {
+        console.log(null);
+        searchTerm = 'pizza';
+      }
       this.transitionToRoute('search.results', searchTerm, 'random');
     }
   }
--- client/tmp/babel-output_path-L2kb99K4.tmp/itpfin/controllers/search.js
+++ client/tmp/babel-output_path-L2kb99K4.tmp/itpfin/controllers/search.js
@@ -8,12 +8,22 @@ define('itpfin/controllers/search', ['exports', 'ember'], function (exports, _em
         console.log('form submitted');
         var searchTerm = this.get('searchTerm');
         var location = this.get('location');
+        if (searchTerm == null) {
+          searchTerm = 'pizza';
+        }
+        if (location == null) {
+          location = 'random';
+        }
         this.transitionToRoute('search.results', searchTerm, location);
       },
       searchRandom: function searchRandom(e) {
         e.preventDefault();
         console.log('random form submitted');
         var searchTerm = this.get('searchTerm');
+        if (searchTerm == null) {
+          console.log(null);
+          searchTerm = 'pizza';
+        }
         this.transitionToRoute('search.results', searchTerm, 'random');
       }
     }
